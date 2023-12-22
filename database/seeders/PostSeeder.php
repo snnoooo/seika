d3f33bc0b764161440cdc1dff580425d8f2cdea8
snnoooo <s.2003-haru@docomo.ne.jp>
2023-12-22 17:48:51 +0900

    マイグレーションとシーダ作成

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'posts_name' => 'リップ',
+            'image_filename' => 'ファイル名',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }
+}
