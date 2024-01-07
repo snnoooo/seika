90d7875f23bfaf06e91c328cc360a531895e46fc
snnoooo <s.2003-haru@docomo.ne.jp>
2024-01-07 20:31:46 +0900

    エラーわからず終了

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,13 @@ class Post extends Model
     use HasFactory;
     
     protected $fillable = [
-        'image_filename', 'posts_name'
+        'title', 'body'
         ];
+
+    public function getByLimit(int $limit_count = 10)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
 }
+}
+
